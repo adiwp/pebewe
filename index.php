4c9890672554bed769280c3032d408835c9b229f
[Adi Wahyu Pribadi] <[adi.wahyu.p@gmail.com]>
2022-11-03 14:43:42 +0700

    proyek versi pertama

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "Halo dunia!";
+
+$x = 1;
+$y = 1.578;
+
+$total = $x + $y;
+
+echo "\n$total";
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "Halo dunia!";
+
+$a = 1;
+$b = 1.578;
+
+$total = $a + $b;
+
+echo "\n$total";
